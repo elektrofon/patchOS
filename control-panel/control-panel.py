b486d3f148a5b442cc7653507fad096af4455364
Rune Warhuus <rune@dinkdonk.no>
2020-04-18 22:13:19 +0200

    Control panel UI work in progress

--- control-panel/control-panel.py
+++ control-panel/control-panel.py
@@ -1,6 +1,9 @@
 import argparse
+from time import sleep
+from threading import Thread, Event
 # from pydbus import SessionBus
 from flask import Flask, render_template, jsonify, request
+from flask_socketio import SocketIO, emit
 
 parser = argparse.ArgumentParser(description='patchOS control panel')
 parser.add_argument('--port', dest='port', type=int, default=80)
@@ -13,6 +16,19 @@ args = parser.parse_args()
 #     print(unit)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
+
+thread = Thread()
+thread_stop_event = Event()
+
+def checkStatusLoop():
+    while not thread_stop_event.isSet():
+        socketio.emit('status', {'data': {'jack': 'running', 'jacktrip': 'stopped'}})
+        socketio.sleep(5)
+
 
 @app.route("/")
 def index():
@@ -20,13 +36,29 @@ def index():
 
     return render_template('index.html', **templateData)
 
+
 @app.route("/jacktrip/start", methods=['POST'])
 def jacktripStart():
     return jsonify(request.json)
+
 
 @app.route("/jacktrip/stop", methods=['POST'])
 def jacktripStop():
     return jsonify(request.json)
 
+
+@socketio.on('connect')
+def onConnect():
+    global thread
+
+    if not thread.isAlive():
+        thread = socketio.start_background_task(checkStatusLoop)
+
+
+@socketio.on('status?')
+def queryStatus():
+    emit('status', {'data': {'jack': 'running', 'jacktrip': 'stopped'}})
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=args.port, debug=True)
+    socketio.run(app, host='0.0.0.0', port=args.port)
--- control-panel/static/js/app.js
+++ control-panel/static/js/app.js
@@ -1,4 +1,72 @@
-function startJacktrip() {
+const connectToServerButton = document.querySelector('.connect-to-server-button');
+const serverIpInput = document.querySelector('input [name="serverIp"]');
+const startServerButton = document.querySelector('.start-server-button');
+const startupViewElement = document.querySelector('.startup-view');
+const serverRunningViewElement = document.querySelector('.server-running-view');
+const clientConnectViewElement = document.querySelector('.client-connect-view');
+
+startupViewElement.style.display = 'none';
+serverRunningViewElement.style.display = 'none';
+clientConnectViewElement.style.display = 'none';
+
+// Mode can be either 'server' or 'client'
+let mode = 'server';
+
+const socket = io(`ws://${location.host}`);
+
+socket.on('connect', () => {
+	console.log('WebSocket connected');
+});
+
+socket.on('disconnect', () => {
+	console.log('WebSocket disconnected');
+});
+
+socket.on('status', (data) => {
+	console.log(data.data.jacktrip);
+
+	if (mode == 'server') {
+		if (data.data.jacktrip == 'running') {
+			startupViewElement.style.display = 'none';
+			serverRunningViewElement.style.display = 'block';
+			clientConnectViewElement.style.display = 'none';
+		} else {
+			startupViewElement.style.display = 'block';
+			serverRunningViewElement.style.display = 'none';
+			clientConnectViewElement.style.display = 'none';
+		}
+	} else if (mode == 'client') {
+
+	} else {
+		console.error('Unknown state');
+	}
+});
+
+socket.emit('status?');
+
+function disableAllInputs() {
+	const elements = document.querySelectorAll('.button, .input');
+
+	for (const element of elements) {
+		element.setAttribute('disabled', '');
+	}
+
+	return elements;
+}
+
+function enableInputs(elements) {
+	for (const element of elements) {
+		element.removeAttribute('disabled');
+	}
+}
+
+function startServer() {
+	mode = 'server';
+
+	startServerButton.classList.add('is-loading');
+	
+	const disabledElements = disableAllInputs();
+
 	fetch('/jacktrip/start', {
 		method: 'POST',
 		headers: {
@@ -9,8 +77,30 @@ function startJacktrip() {
 	.then((response) => response.json())
 	.then((data) => {
 		console.log('Success:', data);
+
+		startServerButton.classList.remove('is-loading');
+		enableInputs(disabledElements);
+
+		startupViewElement.style.display = 'none';
+		serverRunningViewElement.style.display = 'block';
+		clientConnectViewElement.style.display = 'none';
 	})
 	.catch((error) => {
 		console.error('Error:', error);
 	});
 }
+
+function showConnectToServer() {
+	mode = 'client';
+	startupViewElement.style.display = 'none';
+	serverRunningViewElement.style.display = 'none';
+	clientConnectViewElement.style.display = 'block';
+}
+
+function connectToServer() {
+	connectToServerButton.classList.add('is-loading');
+
+	const disabledElements = disableAllInputs();
+}
+
+// serverIpInput. 
